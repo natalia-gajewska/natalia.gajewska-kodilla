f5fc69070208caa7e03e57d32683a0f73600b670
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-03-15 09:05:34 +0100

    changes in class calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
-    }
-}
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(int a = 1, int b=5);
+
+
+        int result = calculator.addAToB();
+
+        if (result ==6) {
+            System.out.println("Jest OK");
+
+        } else {
+
+            System.out.println ("zły wynik");
+        }
+
+        }
+        }
