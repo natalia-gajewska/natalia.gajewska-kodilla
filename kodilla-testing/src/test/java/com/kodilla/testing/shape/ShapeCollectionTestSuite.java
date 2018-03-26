725bfc092ecf4ae5693fe9f27f405195c804ab33
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-03-26 17:04:36 +0200

    changes

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectionTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.shape;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
+public class ShapeCollectionTestSuite {
+
+  public void testShapeCollection(){
+
+      ArrayList<String> shape = new ArrayList<String>();
+
+
+
+
+  }
+}
