7933aed3cb4ad237ecc968f66bf6c81f4f7033e2
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-05-07 10:12:01 +0200

    11.1

---
+++ kodilla-patterns/src/main/java/patterns/singleton/Logger.java
@@ -0,0 +1,26 @@
+package patterns.singleton;
+
+public class Logger {
+    private static Logger loggerInstance = null;
+    private String lastLog = " ";
+
+    private Logger(){
+    }
+
+    public static Logger getInstatnce(){
+        if(loggerInstance == null){
+            loggerInstance = new Logger();
+        }
+        return loggerInstance;
+    }
+
+    public void log(String log){
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
+
+}
---
+++ kodilla-patterns/src/test/java/singleton/LoggerTestSuite.java
@@ -0,0 +1,18 @@
+package singleton;
+
+import org.junit.Assert;
+import org.junit.Test;
+import patterns.singleton.Logger;
+
+public class LoggerTestSuite {
+    @Test
+    public void getLastLogTest(){
+        //Given
+        Logger.getInstatnce().log("Singleton");
+        //When
+        String lastLog = Logger.getInstatnce().getLastLog();
+        //Then
+        Assert.assertEquals("Singleton", lastLog);
+    }
+
+}
