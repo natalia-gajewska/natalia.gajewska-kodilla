6d57cfa194af3207eceaf2b03e5e6dc0fb426556
natalia.gajewska <natalia.gajewska@poczta.fm>
2018-07-20 10:15:43 +0200

    23.4

---
+++ kodilla-testing2/src/main/java/config/TrelloInfoAdmin.java
@@ -0,0 +1,16 @@
+package config;
+
+public class TrelloInfoAdmin {
+
+    private static String login = "login";
+    private static String password = "haslo";
+
+    public static String getLogin() {
+        return login;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+}
+
